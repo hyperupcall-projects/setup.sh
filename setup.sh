1754a959534df6dcdfe0650465ab6439ebb3b33f
Edwin Kofler <edwin@kofler.dev>
2026-03-30 19:21:29 -0700

    Fix installing from source to actually work

--- setup.sh
+++ setup.sh
@@ -138,26 +138,6 @@ EOF
 		rm -rf "$g_temp_dir3"
 	fi
 
-	# Install from source if install.source function exists.
-	if declare -f 'install.source' &>/dev/null; then
-		core.print_info "Installing \"$g_name\" from source..."
-		local orig_dir4="$PWD"
-		g_temp_dir4=$(mktemp -d --suffix "-dotfiles")
-		cd "$g_temp_dir4"
-		_setup_cleanup4() {
-			cd /
-			rm -rf "$g_temp_dir4"
-		}
-		core.trap_add '_setup_cleanup4' ERR EXIT
-
-		(
-			install.source "$@"
-		)
-
-		cd "$orig_dir4"
-		rm -rf "$g_temp_dir4"
-	fi
-
 	if [ "$flag_configure_only" = 'no' ]; then
 		if installed && [ "$flag_force" = no ]; then
 			core.print_info "Program \"$g_name\" already installed"
@@ -242,10 +222,6 @@ util.install_by_setup() {
 		local id=
 		for id in source "$ID" "$ID_LIKE" any; do
 			if declare -f "$flag_fn_prefix.$id" &>/dev/null; then
-				if [ "$id" = 'source' ] && [ "$flag_dev" != true ]; then
-					break
-				fi
-
 				ran_function=yes
 				if command -v installed &>/dev/null; then
 					if ! installed || [ "$flag_force" = 'yes' ]; then
